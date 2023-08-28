9ed1796fb5c0d72de1decffb272185126c7c8f15
Guen <heloo@guen.dev>
2023-08-28 22:53:07 +0700

    Initial Commit

---
+++ swift-async-await/swift_async_awaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_async_awaitApp.swift
+//  swift-async-await
+//
+//  Created by Guen on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct swift_async_awaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
